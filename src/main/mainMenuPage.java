999abaf99c51d620bafcbb9bf35f5f6a8ada8d68
hena <henap741@gmail.com>
2023-02-12 23:13:59 -0500

    functions added

--- src/main/mainMenuPage.java
+++ src/main/mainMenuPage.java
@@ -1,24 +1,105 @@
+//package main;
+//
+//import java.awt.EventQueue;
+//
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.border.EmptyBorder;
+//import javax.swing.JInternalFrame;
+//import javax.swing.JSeparator;
+//import javax.swing.SwingConstants;
+//import javax.swing.JMenuBar;
+//import javax.swing.JSplitPane;
+//import javax.swing.JToggleButton;
+//import java.awt.Font;
+//import javax.swing.JLabel;
+//import javax.swing.JButton;
+//
+//public class mainMenu extends JFrame {
+//
+//	private JPanel contentPane;
+//
+//	/**
+//	 * Launch the application.
+//	 */
+//	
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					mainMenu frame = new mainMenu();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+//	
+//	/**
+//	 * Create the frame.
+//	 */
+//	public mainMenu() {
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setBounds(100, 100, 450, 300);
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//
+//		setContentPane(contentPane);
+//		contentPane.setLayout(null);
+//		
+//		JToggleButton settings_button = new JToggleButton("Settings");
+//		settings_button.setBounds(10, 231, 110, 23);
+//		settings_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+//		contentPane.add(settings_button);
+//		
+//		JLabel menu_label = new JLabel("Menu");
+//		menu_label.setBounds(274, 11, 48, 14);
+//		menu_label.setFont(new Font("Times New Roman", Font.BOLD, 16));
+//		contentPane.add(menu_label);
+//		
+//		JButton course_lookup_button = new JButton("Course Lookup");
+//		course_lookup_button.setBounds(201, 53, 196, 23);
+//		course_lookup_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+//		contentPane.add(course_lookup_button);
+//		
+//		JButton degree_checklist_button = new JButton("Degree Checklist");
+//		degree_checklist_button.setBounds(201, 102, 196, 23);
+//		degree_checklist_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+//		contentPane.add(degree_checklist_button);
+//		
+//		JButton suggested_path_button = new JButton("Suggested Path");
+//		suggested_path_button.setBounds(201, 152, 196, 23);
+//		suggested_path_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+//		contentPane.add(suggested_path_button);
+//		
+//		JButton faculty_info_button = new JButton("Faculty Information");
+//		faculty_info_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+//		faculty_info_button.setBounds(201, 200, 196, 23);
+//		contentPane.add(faculty_info_button);
+//	}
+//}
+
 package main;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JInternalFrame;
-import javax.swing.JSeparator;
-import javax.swing.SwingConstants;
-import javax.swing.JMenuBar;
-import javax.swing.JSplitPane;
 import javax.swing.JToggleButton;
 import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
-public class mainMenu extends JFrame {
+public class mainMenuPage extends JFrame {
 
 	private JPanel contentPane;
-
+	private cousrseLookupPage courseLookupPage;
+	private facultyInfoPage facultyInfoPage;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -27,7 +108,7 @@ public class mainMenu extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					mainMenu frame = new mainMenu();
+					mainMenuPage frame = new mainMenuPage();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +120,7 @@ public class mainMenu extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public mainMenu() {
+	public mainMenuPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -58,19 +139,41 @@ public class mainMenu extends JFrame {
 		menu_label.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		contentPane.add(menu_label);
 		
-		JButton course_lookup_label = new JButton("Course Lookup");
-		course_lookup_label.setBounds(201, 50, 196, 23);
-		course_lookup_label.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		contentPane.add(course_lookup_label);
+		JButton course_lookup_button = new JButton("Course Lookup");
+		course_lookup_button.setBounds(201, 53, 196, 23);
+		course_lookup_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		course_lookup_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				courseLookupPage = new cousrseLookupPage();
+				courseLookupPage.setVisible(true);
+				setVisible(false);
+			}
+		});
+		contentPane.add(course_lookup_button);
 		
-		JButton degree_checklist_label = new JButton("Degree Checklist");
-		degree_checklist_label.setBounds(201, 105, 196, 23);
-		degree_checklist_label.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		contentPane.add(degree_checklist_label);
+		JButton degree_checklist_button = new JButton("Degree Checklist");
+		degree_checklist_button.setBounds(201, 102, 196, 23);
+		degree_checklist_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		contentPane.add(degree_checklist_button);
 		
-		JButton suggested_path_label = new JButton("Suggested Path");
-		suggested_path_label.setBounds(201, 155, 196, 23);
-		suggested_path_label.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		contentPane.add(suggested_path_label);
+		JButton suggested_path_button = new JButton("Suggested Path");
+		suggested_path_button.setBounds(201, 152, 196, 23);
+		suggested_path_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		contentPane.add(suggested_path_button);
+		
+		JButton faculty_info_button = new JButton("Faculty Information");
+		faculty_info_button.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		faculty_info_button.setBounds(201, 200, 196, 23);
+		faculty_info_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				facultyInfoPage = new facultyInfoPage();
+				facultyInfoPage.setVisible(true);
+				setVisible(false);
+			}
+		});
+		contentPane.add(faculty_info_button);
 	}
 }
+
