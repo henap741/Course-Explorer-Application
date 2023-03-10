e442aeb98faa3be1b1a8e65094b02ffaa96488f5
Daoud Ali <65147757+DaoudAli@users.noreply.github.com>
2023-03-10 01:43:14 -0500

    auth refactor 2

--- src/main/BusinessLogic/Auth/auth.java
+++ src/main/BusinessLogic/Auth/auth.java
@@ -6,12 +6,18 @@ import main.DB.util.objects.userObject;
 public class auth {
 
     public Boolean attemptLogin(String username, String password) {
-        DB queryDB = new DB();
-        userObject userFound = queryDB.getUser(username);
-        if (userFound.getPassword() == password) {
-            return true;
+        try {
+            DB queryDB = new DB();
+            userObject userFound = queryDB.getUser(username);
+            if (userFound.getPassword() == password) {
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error logging in - auth.java");
+            return false;
         }
-        return false;
+
     }
 
     public Boolean signUp(String username, String password) {
