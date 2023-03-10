a6289aa5a5776002e2a23706d24f1cd9555c93d5
hena <henap741@gmail.com>
2023-03-09 23:41:07 -0500

    More Classes added

--- src/main/BusinessLogic/ProgramPathway/OsgoodeLawSchool.java
+++ src/main/BusinessLogic/ProgramPathway/OsgoodeLawSchool.java
@@ -13,6 +13,9 @@ public class OsgoodeLawSchool {
 	        requiredCourses1.add(new courses("LAW 1000", "Introduction to Canadian Law", "First Year", "4", "Fall", "An introduction to the Canadian legal system and the role of law in Canadian society.", null, "Prof. Smith"));
 	        requiredCourses1.add(new courses("LAW 2000", "Contracts", "Second Year", "3", "Fall", "An exploration of the legal principles governing contracts, including formation, interpretation, and remedies for breach.", null, "Prof. Jones"));
 	        requiredCourses1.add(new courses("LAW 3000", "Torts", "First Year", "4", "Winter", "An examination of the law of torts, including negligence, intentional torts, and strict liability.", null, "Prof. Brown"));
+	        requiredCourses1.add(new courses("LAW1004", "Criminal Law", "First Year", "3", "Winter", "An introduction to criminal law", null, "Prof. Linken"));
+	        requiredCourses1.add(new courses("LAW1005", "Legal Process", "First Year", "3", "Fall", "An introduction to the legal process", null, "Prof. Wesley"));
+	        requiredCourses1.add(new courses("LAW1006", "Public Law and Legislation", "3", "Winter", "First Year", "An introduction to public law and legislation", null, "Prof. James"));
 	        degreePrograms.add(new pathwayCalculator("Juris Doctor (JD)", "4", "This degree is for Juris Doctor (JD) ", "158", requiredCourses1));
 	        
 	        ArrayList<courses> requiredCourses2 = new ArrayList<courses>();
